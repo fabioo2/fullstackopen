5ac991ad7ce5a93999331f1fc0bd67eaf6d1a935
Fabio Kim <fabio.kim@d2l.com>
2021-03-10 12:27:06 -0500

    accidentally pushed api key, created new key and added env variable

--- part2/countries/src/components/Weather.js
+++ part2/countries/src/components/Weather.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const api_key = process.env.REACT_APP_API_KEY;
+
 const Weather = ({ city }) => {
     const [weather, setWeather] = useState({
         temperature: 0,
@@ -11,18 +13,17 @@ const Weather = ({ city }) => {
     });
 
     const hook = () => {
-        axios
-            .get(`http://api.weatherstack.com/current?access_key=34a0a91617253ede7a39ad3116054ec5&query=${city}`)
-            .then((response) => {
-                const newWeatherObject = {
-                    temperature: response.data.current.temperature,
-                    weather_icon: response.data.current.weather_icons[0],
-                    weather_description: response.data.current.weather_descriptions,
-                    wind_speed: response.data.current.wind_speed,
-                    wind_direction: response.data.current.wind_dir
-                };
-                setWeather(newWeatherObject);
-            });
+        console.log(api_key);
+        axios.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${city}`).then((response) => {
+            const newWeatherObject = {
+                temperature: response.data.current.temperature,
+                weather_icon: response.data.current.weather_icons[0],
+                weather_description: response.data.current.weather_descriptions,
+                wind_speed: response.data.current.wind_speed,
+                wind_direction: response.data.current.wind_dir
+            };
+            setWeather(newWeatherObject);
+        });
     };
 
     useEffect(hook, [city]);
